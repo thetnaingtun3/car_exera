55fe1d16518056d5d6e677efdd90724c58032571
msi <thatnaing336@gmail.com>
2025-02-21 08:15:44 +0630

    layout change

--- resources/views/car_qr_date_change.blade.php
+++ resources/views/car_qr_date_change.blade.php
@@ -59,20 +59,35 @@
         </div>
 
 
+
+
         <form action="{{ route('car-qr-date-change', ['ids' => implode(',', $carIds)]) }}" method="post">
+
             @csrf
-            <input type="hidden" name="selectedCars" value="{{ json_encode($selectedCars) }}">
-            <div>
-                <label class="block text-sm font-medium text-gray-700">
-                    Registered Date
-                    (MM/DD/YYYY)</label>
-                <input name="click_date" type="date"
-                    class="block w-full p-2 mt-1 text-sm border rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
-            </div>
-            <div class="mt-4 px-4">
-                <a href="{{ url()->previous() }}" class="inline-block px-4 py-2 ml-2 text-white bg-blue-500 rounded">Go
-                    Back</a>
-                <button type="submit" class="px-4 py-2 text-white bg-green-500 rounded">Submit</button>
+            <div class="flex flex-wrap mt-8">
+                <!-- Select LSP -->
+
+                <div class="flex flex-wrap gap-2">
+
+
+                    <input type="hidden" name="selectedCars" value="{{ json_encode($selectedCars) }}">
+
+                    <div class="w-full lg:w-6/12">
+                        <div class="w-full py-2">
+                            <label class="block text-sm font-medium text-gray-700">
+                                Registered Date
+                                (MM/DD/YYYY)</label>
+                            <input name="click_date" type="date"
+                                class="block w-full p-2 mt-1 text-sm border rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
+                        </div>
+                    </div>
+                    <div class="mt-4 px-4">
+                        <a href="{{ url()->previous() }}"
+                            class="inline-block px-4 py-2 ml-2 text-white bg-blue-500 rounded">Go
+                            Back</a>
+                        <button type="submit" class="px-4 py-2 text-white bg-green-500 rounded">Submit</button>
+                    </div>
+                </div>
             </div>
 
         </form>
